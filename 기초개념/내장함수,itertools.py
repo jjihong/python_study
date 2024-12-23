89ec6b6de79d16560f511bdc823d1d71acb2d91d
jihong <hongs7803@gmail.com>
2024-12-23 13:01:54 +0900

    간략한 수정

--- 기초개념/내장함수,itertools.py
+++ 기초개념/내장함수,itertools.py
@@ -1,37 +1,3 @@
-a = [1,4,3]
-print(f"기본 리스트 : {a}")
-
-a.append(2)
-print(f"삽입 : {a}")
-
-a.sort()
-print(f"오름차순 정렬 : {a}")
-
-a.sort(reverse=True)
-print(f"내림차순 정렬 : {a}")
-
-a.reverse()
-print(f"원소 뒤집기 : {a}")
-
-a.insert(2,3)
-print(f"인덱스 2에 3추가 : {a}")
-
-print(f"값이 3인 데이터개수 : {a.count(3)}")
-
-a.remove(1)
-print(f"값이 1인 데이터 삭제 : {a}")
-
-# append 시간 복잡도는 O(1)
-# insert append보다 느림
-# remove insert와 동일 따라서 남발하면 실행 시간이 느려짐
-
-a = [1,2,3,4,5,5,5]
-remove_set = [3,5]
-
-# remove_set에 포함되지 않는 값만 저장
-result = [i for i in a if i not in remove_set]
-print(result)
-
 # (2) itertools
 # 반복되는 데이터 처리에 유용, 대표적으로 permutations, combinations
 
